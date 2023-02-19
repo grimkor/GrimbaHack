8b22af8edadadf647db2333960408e6aacefe627
Grimbakor <david@grimkor.tech>
2023-02-19 13:09:33 +0000

    feat: Collision viewer starts when checked, disables automatically when leaving training mode and tracks last value

--- Modules/CollisionBoxViewer.cs
+++ Modules/CollisionBoxViewer.cs
@@ -1,4 +1,5 @@
 using System;
+using GrimbaHack.Utility;
 using HarmonyLib;
 using nway.collision;
 using nway.gameplay;
@@ -12,13 +13,13 @@ namespace GrimbaHack.Modules;
 
 public class CollisionBoxViewer : ModuleBase
 {
-    private static ColliderRenderer colliderRenderer { get; set; }
-
     public static CollisionBoxViewer Instance { get; private set; }
 
     static CollisionBoxViewer()
     {
-        Instance = new CollisionBoxViewer();
+        Instance = new();
+        OnEnterTrainingMatchActionHandler.Instance.AddCallback(() => Instance.Enabled = Instance._enabled);
+        OnEnterMainMenuActionHandler.Instance.AddCallback(() => CollisionBoxViewerController.Enabled = false);
     }
 
     private static bool _cameraInitialised;
@@ -30,46 +31,11 @@ public class CollisionBoxViewer : ModuleBase
         get => _enabled;
         set
         {
+            CollisionBoxViewerController.Enabled = value;
             _enabled = value;
-            if (colliderRenderer)
-            {
-                colliderRenderer.enabled = value;
-            }
         }
     }
 
-    [HarmonyPatch(typeof(SimulationManager), nameof(SimulationManager.Initialize))]
-    public class PatchCollisionShit
-    {
-        public static void Postfix()
-        {
-            if (!Instance.Enabled) return;
-            if (!_cameraInitialised)
-            {
-                var stackCamera = GameObject.FindObjectOfType<StackCamera>();
-                if (stackCamera == null) return;
-                colliderRenderer = stackCamera.gameObject.AddComponent<ColliderRenderer>();
-                colliderRenderer.isGlobalSleep = true;
-                _cameraInitialised = true;
-            }
-
-            SceneStartup.instance.GamePlay._playerList[0].characterTeam.members
-                .ForEach(new Action<Character>(new Action<Character>(member =>
-                {
-                    member.renderColliderList.Clear();
-                })));
-            SceneStartup.instance.GamePlay._playerList[0].renderColliderList = ColliderManager.instance.core.colliders;
-        }
-    }
-
-    [HarmonyPatch(typeof(MatchManager), nameof(MatchManager.SetupGamePlay))]
-    public class PatchSetupGamePlay
-    {
-        public static void Postfix()
-        {
-            _cameraInitialised = false;
-        }
-    }
 
     public static void CreateUIControls(GameObject contentRoot)
     {
@@ -84,5 +50,94 @@ public class CollisionBoxViewer : ModuleBase
         collisionBoxViewerToggle.onValueChanged.AddListener(new Action<bool>((value) => { Instance.Enabled = value; }));
 
         UIFactory.SetLayoutElement(collisionBoxViewerToggle.gameObject, minHeight: 25, minWidth: 50);
+    }
+}
+
+public static class CollisionBoxViewerController
+{
+    private static bool _enabled;
+
+    public static bool Enabled
+    {
+        get => _enabled;
+        set
+        {
+            _enabled = value;
+            if (value)
+            {
+                InitCamera();
+                SetCamera(true);
+                MapColliders();
+            }
+            else
+            {
+                SetCamera(false);
+                ClearColliders();
+            }
+        }
+    }
+
+    public static bool _cameraInitialised;
+    private static ColliderRenderer colliderRenderer { get; set; }
+
+    public static void InitCamera()
+    {
+        if (!_cameraInitialised)
+        {
+            var stackCamera = GameObject.FindObjectOfType<StackCamera>();
+            if (stackCamera == null) return;
+            colliderRenderer = stackCamera.gameObject.AddComponent<ColliderRenderer>();
+            colliderRenderer.isGlobalSleep = true;
+            _cameraInitialised = true;
+        }
+    }
+
+    public static void SetCamera(bool enable)
+    {
+        if (colliderRenderer)
+        {
+            colliderRenderer.enabled = enable;
+        }
+    }
+
+    public static void ClearColliders()
+    {
+        if (SceneStartup.instance == null) return; 
+        SceneStartup.instance.GamePlay._playerList[0].characterTeam.members
+            .ForEach(new Action<Character>(new Action<Character>(member => { member.renderColliderList.Clear(); })));
+    }
+
+    public static void MapColliders()
+    {
+        ClearColliders();
+        SceneStartup.instance.GamePlay._playerList[0].renderColliderList = ColliderManager.instance.core.colliders;
+    }
+
+    [HarmonyPatch(typeof(SimulationManager), nameof(SimulationManager.Initialize))]
+    public class PatchCollisionShit
+    {
+        public static void Postfix()
+        {
+            if (Enabled)
+            {
+                InitCamera();
+                MapColliders();
+            }
+            else
+            {
+                ClearColliders();
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(MatchManager), nameof(MatchManager.SetupGamePlay))]
+    public class PatchSetupGamePlay
+    {
+        public static void Postfix()
+        {
+            _cameraInitialised = false;
+            if (Enabled)
+                InitCamera();
+        }
     }
 }
