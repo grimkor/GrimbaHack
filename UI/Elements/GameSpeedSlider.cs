108388aa1006d4b85ea51c30c9a904d34484e60f
david <davidstur@gmail.com>
2024-03-15 09:05:21 +0000

    Moved listener and ButtonBarConfig logic internally to GameSpeedSlider

--- UI/Elements/GameSpeedSlider.cs
+++ UI/Elements/GameSpeedSlider.cs
@@ -1,13 +1,17 @@
 using System;
 using GrimbaHack.Modules;
 using GrimbaHack.UI.MenuItems;
+using nway.gameplay.ui;
 using nway.ui;
+using UnityEngine.Events;
 
 namespace GrimbaHack.UI.Elements;
 
 public static class GameSpeedSlider
 {
-    public static BetterRangeSelector Generate(MenuPage menuPage)
+    private static bool _selected;
+
+    public static void Generate(MenuPage menuPage, ButtonBarConfig buttonBarConfig, UIWindow window)
     {
         var rangeSelector =
             menuPage.Page.AddItem<BetterRangeSelector>("gameSpeedRangeSelector");
@@ -16,6 +20,28 @@ public static class GameSpeedSlider
         rangeSelector.MaxValue = 100;
         rangeSelector.CurrentValue = SimulationSpeed.GetSpeed();
         rangeSelector.OnValueChanged = (Action<int, int>)((newValue, _) => { SimulationSpeed.SetSpeed(newValue); });
-        return rangeSelector;
+        rangeSelector.selectable.SetOnSelect((Action<ILayeredEventData>)(eventData =>
+        {
+            _selected = true;
+            buttonBarConfig.SetLocalizedText(ButtonBarItem.ButtonY, "Reset Value");
+            nway.gameplay.ui.UIManager.Get.ButtonBar.Update(eventData.Input, UserPersistence.Get.p1ButtonMap,
+                buttonBarConfig);
+        }));
+        rangeSelector.selectable.SetOnDeselect((Action<ILayeredEventData>)(eventData =>
+        {
+            _selected = false;
+            buttonBarConfig.ClearText(ButtonBarItem.ButtonY);
+            nway.gameplay.ui.UIManager.Get.ButtonBar.Update(eventData.Input, UserPersistence.Get.p1ButtonMap,
+                buttonBarConfig);
+        }));
+        window.AddButtonCallback(MenuButton.XboxY, (UnityAction<ILayeredEventData>)((ILayeredEventData eventData) =>
+        {
+            if (_selected)
+            {
+                rangeSelector.CurrentValue = 100;
+                rangeSelector.slider.Set(1);
+                SimulationSpeed.SetSpeed(100);
+            }
+        }));
     }
 }
--- UI/Pages/GrimUITrainingModeSettings.cs
+++ UI/Pages/GrimUITrainingModeSettings.cs
@@ -1,8 +1,6 @@
 using System;
-using GrimbaHack.Modules;
 using GrimbaHack.UI.Elements;
 using GrimbaHack.UI.MenuItems;
-using GrimbaHack.Utility;
 using nway.gameplay.ui;
 using nway.ui;
 
@@ -11,14 +9,7 @@ namespace GrimbaHack.UI.Pages
     public class GrimUITrainingModeSettings : GrimUIPage
     {
         private UIPage Page => Menu?.Page;
-        private MenuRangeSelector _rangeSelector;
-        private TrainingModeSelectables _currentlySelected;
         private bool _listenersSetup;
-
-        private void SetCurrentlySelected(TrainingModeSelectables selectable)
-        {
-            _currentlySelected = selectable;
-        }
 
         public GrimUITrainingModeSettings()
         {
@@ -33,21 +24,6 @@ namespace GrimbaHack.UI.Pages
             }
 
             _listenersSetup = true;
-            Window.AddButtonCallback(MenuButton.XboxY, (Action<ILayeredEventData>)(_ =>
-                    {
-                        switch (_currentlySelected)
-                        {
-                            case TrainingModeSelectables.GameSpeed:
-                            default:
-                                _rangeSelector.CurrentValue = 100;
-                                _rangeSelector.slider.Set(1);
-                                SimulationSpeed.SetSpeed(100);
-                                break;
-                            case TrainingModeSelectables.None:
-                                break;
-                        }
-                    }
-                ));
             Menu.Page.SetOnShow((Action)SetupButtonBar);
             Menu.Page.SetOnHide((Action)(() =>
             {
@@ -55,7 +31,6 @@ namespace GrimbaHack.UI.Pages
                 nway.gameplay.ui.UIManager.Get.ButtonBar.Update(ControllerManager.GetController(0),
                     UserPersistence.Get.p1ButtonMap,
                     ButtonBarConfig);
-                SetCurrentlySelected(TrainingModeSelectables.None);
             }));
         }
 
@@ -65,21 +40,7 @@ namespace GrimbaHack.UI.Pages
             CollisionBoxViewerSelector.Generate(Menu);
             ShowFrameDataSelector.Generate(Menu);
             UnlimitedInstallSelector.Generate(Menu);
-            _rangeSelector = GameSpeedSlider.Generate(Menu);
-            _rangeSelector.selectable.SetOnSelect((Action<ILayeredEventData>)(eventData =>
-            {
-                SetCurrentlySelected(TrainingModeSelectables.GameSpeed);
-                ButtonBarConfig.SetLocalizedText(ButtonBarItem.ButtonY, "Reset Value");
-                nway.gameplay.ui.UIManager.Get.ButtonBar.Update(eventData.Input, UserPersistence.Get.p1ButtonMap,
-                    ButtonBarConfig);
-            }));
-            _rangeSelector.selectable.SetOnDeselect((Action<ILayeredEventData>)(eventData =>
-            {
-                SetCurrentlySelected(TrainingModeSelectables.None);
-                ButtonBarConfig.ClearText(ButtonBarItem.ButtonY);
-                nway.gameplay.ui.UIManager.Get.ButtonBar.Update(eventData.Input, UserPersistence.Get.p1ButtonMap,
-                    ButtonBarConfig);
-            }));
+            GameSpeedSlider.Generate(Menu, ButtonBarConfig, Window);
             BackButton.Create(Menu, () =>
             {
                 ButtonBarConfig.SetLocalizedText(ButtonBarItem.ButtonRB, "Extra Training Options");
@@ -90,7 +51,6 @@ namespace GrimbaHack.UI.Pages
                     ButtonBarConfig);
                 GoBackCallback();
             });
-
         }
 
         public void Hide(bool parentClosing)
@@ -101,12 +61,6 @@ namespace GrimbaHack.UI.Pages
             }
 
             Hide();
-        }
-
-        public override void Hide()
-        {
-            SetCurrentlySelected(TrainingModeSelectables.None);
-            base.Hide();
         }
 
         public void SetupButtonBar()
