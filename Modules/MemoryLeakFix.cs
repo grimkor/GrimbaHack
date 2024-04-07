4b36c3d9cb253b1cd3fce01da0a18441d3cae7f4
david <davidstur@gmail.com>
2024-04-07 10:07:11 +0100

    Revert MemoryLeakFix to old method

--- Modules/MemoryLeakFix.cs
+++ Modules/MemoryLeakFix.cs
@@ -25,19 +25,31 @@ public class MemoryLeakFix
             {
                 return;
             }
-            Plugin.Log.LogInfo("Running Memory Leak Fix");
+
             var go = Object.FindObjectsOfType<Material>();
-            go
-                .GroupBy(x => x.name)
-                .Where(grouping => grouping.Count() > 20)
-                .ToList()
-                .ForEach(duplicate =>
+            foreach (var material in go)
+            {
+                if (!material.name.Contains("vfx_m"))
                 {
-                    foreach (var material in duplicate)
-                    {
-                        Object.Destroy(material);
-                    }
-                });
+                    continue;
+                }
+
+                Object.Destroy(material);
+            }
+
+            // This has the issue that it removes the EX flash, turns out that might also be a leak
+            // or it tidies up properly between loads and needs a rule to skip to not remove it wrongly
+            // go
+            //     .GroupBy(x => x.name)
+            //     .Where(grouping => grouping.Count() > 20)
+            //     .ToList()
+            //     .ForEach(duplicate =>
+            //     {
+            //         foreach (var material in duplicate)
+            //         {
+            //             Object.Destroy(material);
+            //         }
+            //     });
         });
     }
 }
